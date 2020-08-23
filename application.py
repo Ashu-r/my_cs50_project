0beb1dd74ecb7be41632548ce4978b94ee06b75c
ashu-r <randiveashutosh@gmail.com>
2020-08-23 21:00:09 +0530

    bug

--- application.py
+++ application.py
@@ -80,10 +80,10 @@ for code in default_exceptions:
 
 # This only works on linux machines
 
-# if __name__ == '__main__':
-#     app.debug = True
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 # This is for windows machines
 # serve(app,host='0.0.0.0' ,port=8080)
